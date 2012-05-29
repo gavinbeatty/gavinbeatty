4725f91fe99fc5261287fd473f21749d3021170b
Gavin Beatty <gavinbeatty@gmail.com>
2012-05-29 23:04:47 +0200

    small improvements (like TERM_SLEEP)

--- scripts/common/term.sh
+++ scripts/common/term.sh
@@ -2,7 +2,7 @@
 # vi: set ft=sh expandtab tabstop=4 shiftwidth=4:
 
 ###########################################################################
-# Copyright (c) 2011 Gavin Beatty                     
+# Copyright (c) 2011, 2012 Gavin Beatty                     
 # <gavinbeatty@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining
@@ -24,93 +24,82 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ###########################################################################
-
 set -e
 set -u
-
 prog="$(basename -- "$0")"
-prefix="${GAV_PREFIX:-$HOME}"
-lf="
-"
-getopt="${getopt-getopt}"
-verbose="${verbose-0}"
+getopt="${TERM_GETOPT-getopt}"
+help="${TERM_HELP:-}"
+verbose="${TERM_VERBOSE-0}"
+dryrun="${TERM_DRYRUN:-}"
+sleep="${TERM_SLEEP:-2}"
 
-dryrun="${dryrun:-}"
-tmpdir="${tmpdir:-${HOME}/.tmp}"
+tmpdir="${TMPDIR:-${HOME}/.tmp}"
 
-TEXTDOMAIN="$prog" ; export TEXTDOMAIN
-TEXTDOMAINDIR="${prefix}/share/locale/sh/" ; export TEXTDOMAINDIR
-have() { type -- "$@" >/dev/null 2>&1 ; }
-if ! have gettext.sh ; then
-    if (! have fallback_eval_gettext) || (! have fallback_eval_ngettext) ; then
-        echo "$(gettext "error: fallback_eval_(n)gettext are required.")"
-        # don't exit: they mightn't be used
-    fi
-    gettext() { echo "$1" ; }
-    eval_gettext() { fallback_eval_gettext "$@" ; }
-    ngettext() { if test "$3" = "1" ; then echo "$1" ; else echo "$2" ; fi ; }
-    eval_ngettext() { fallback_eval_ngettext "$@" ; }
-fi
+justdo="justdo"
+cleardo="justdo"
 
-set -e
-set -u
-
-usage() {
-    echo "$(eval_gettext "usage: \${prog} <session-name>")"
-}
+usage() { echo "usage: $prog <session-name>" ; }
 help() {
     cat <<EOF
-$(gettext "Copyright (c) 2011 Gavin Beatty <gavinbeatty@gmail.com>")
-$(gettext "Licensed under the MIT license: http://www.opensource.org/licenses/MIT")
+Copyright (c) 2011, 2012 Gavin Beatty <gavinbeatty@gmail.com>
+Licensed under the MIT license: http://www.opensource.org/licenses/MIT
 
-$(gettext "starts a terminal session using dtach and dvtm.")
+starts a terminal session using dtach and dvtm.
 
 $(usage)
-$(gettext "Options:")
+Options:
  -h:
-  $(gettext "Prints this help message.")
+  Prints this help message.
  -v:
-  $(gettext "Give verbose information about what the script is doing.\${lf}For example, print the commands used to do it's work. Supply this\${lf}option multiple times to increase verbosity further.")
+  Give verbose information about what the script is doing.
+  For example, print the commands used to do it's work. Supply this
+  option multiple times to increase verbosity further.
 EOF
 }
-echodo() { echo "$@" ; sleep 1 ; "$@" ; }
-dodo() { "$@" ; }
+have() { type -- "$@" >/dev/null 2>&1 ; }
+echodo() { echo "$@" ; "$@" ; }
+echosleepdo() { echo "$@" ; sleep "$sleep" ; "$@" ; }
+justdo() { "$@" ; }
 
 main() {
-#     check_deps have sleep
-#     check_deps have_file "${prefix}/somefile.txt"
-
-    opts="$("$getopt" -n "$prog" -o "hvnt:" -- "$@")"
-    eval set -- "$opts"
-
-    echoecho="dodo"
-    echodo="dodo"
-    while true ; do
-        case "$1" in
-            -h) help ; exit 0 ; ;;
-            -v) verbose=$(( $verbose + 1 )) ; ;;
-            -n) dryrun=1 ; verbose=1 ; ;;
-            -t) tmpdir="$2" ; shift ; ;;
-            --) shift ; break ; ;;
-            *) echo "error: unknown option $1" >&2 ; exit 1 ; ;;
-        esac
-        shift
-    done
-    if test $verbose -gt 0 ; then
-        echodo="echodo"
-        echoecho="echo"
+    if "$getopt" -n "$prog" -o "fooa:bc:" -- -a b -b -ofo >/dev/null 2>&1 ; then
+        opts="$("$getopt" -n "$prog" -o "hvnt:" -- "$@")"
+        eval set -- "$opts"
+        while true ; do
+            case "$1" in
+                -h) help=1 ;;
+                -v) verbose=$(( $verbose + 1 )) ;;
+                -n) dryrun=1 ; verbose=1 ;;
+                -t) tmpdir="$2" ; shift ;;
+                --) shift ; break ;;
+                *) echo "error: unknown option $1" >&2 ; exit 1 ;;
+            esac
+            shift
+        done
+    else
+        warning "Only taking options from the environment."
     fi
-    if test -n "$dryrun" ; then echodo="echo" ; fi
+    test -z "$help" || { help ; exit 0 ; }
+
+    if test $verbose -gt 0 ; then
+        justdo="echodo"
+        cleardo="echosleepdo"
+    fi
+    if test -n "$dryrun" ; then cleardo="echo" ; fi
     if test $# -eq 0 ; then
         session_name="$(id -un)"
     else
         session_name="$1"
     fi
+    ## dtach-dvtm implementation
     #$echodo dtach -A "${tmpdir}/${session_name}" -e '^B' -r winch dvtm
     e=0
-    $echoecho tmux has-session -t "$session_name" 2>/dev/null || e=$?
-    if test $e -eq 0 ; then $echodo tmux attach -t "$session_name"
-    else $echodo tmux new -s "$session_name" ; fi
+    $justdo tmux has-session -t "$session_name" 2>/dev/null || e=$?
+    if test $e -eq 0 ; then
+        $cleardo tmux attach -t "$session_name"
+    else
+        $cleardo tmux new -s "$session_name"
+    fi
 }
 trap " echo Caught SIGINT >&2 ; exit 1 ; " INT
 trap " echo Caught SIGTERM >&2 ; exit 1 ; " TERM
