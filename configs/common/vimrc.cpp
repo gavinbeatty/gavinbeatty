10d50e3b9d85e1d29062ff29b757d0fe37578076
Gavin Beatty <gavinbeatty@gmail.com>
2012-06-07 15:57:38 +0200

    lots of changes in vimrc, coming from use of solarized, merging and being less dumb

--- configs/common/vimrc.cpp
+++ configs/common/vimrc.cpp
@@ -1,10 +1,8 @@
-
+" vi: set ft=vim expandtab tabstop=4 shiftwidth=4:
 set formatoptions=croql
 iab #d #define
 iab #i #include
 iab #w #warning
 iab #e #error
 execute 'iab #m main(int argc, char* argv[])\n{\n\n}\n\n\n'
-
 set cindent
-
---
+++ configs/common/vimrc.sh
@@ -0,0 +1,1 @@
+" vi: set ft=vim expandtab tabstop=4 shiftwidth=4:
