9a1d97b0262751c792b8fa8e74484633d2dea9e7
Gavin Beatty <gavinbeatty@gmail.com>
2012-06-08 09:46:26 +0200

    if not loaded should be around augroup (not inside), merge vimrc.cpp

--- configs/common/vimrc.cpp
+++ configs/common/vimrc.cpp
@@ -1,8 +1,21 @@
 " vi: set ft=vim expandtab tabstop=4 shiftwidth=4:
-set formatoptions=croql
+setlocal formatoptions=croql
+setlocal cindent
+setlocal makeprg=TERM=dumb\ bjam\ -j6\ link=static
+setlocal makeef=bjam-build-errors.log
 iab #d #define
 iab #i #include
 iab #w #warning
 iab #e #error
 execute 'iab #m main(int argc, char* argv[])\n{\n\n}\n\n\n'
-set cindent
+if has("autocmd")
+    if !exists('cpp_fswitch_augroup')
+        let cpp_fswitch_augroup = 1
+        augroup cpp_fswitch
+            autocmd BufEnter *.cpp let b:fswitchdst = 'hpp,h,hh' | let b:fswitchlocs = '.'
+            autocmd BufEnter *.cc let b:fswitchdst = 'h,hh,hpp' | let b:fswitchlocs = '.'
+            autocmd BufEnter *.hpp let b:fswitchdst = 'cpp' | let b:fswitchlocs = '.'
+            autocmd BufEnter *.h let b:fswitchdst = 'cc,cpp,c' | let b:fswitchlocs = '.'
+        end
+    endif
+endif
