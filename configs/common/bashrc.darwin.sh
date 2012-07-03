1a0292148928aab2b4d2181a2767035020e775b9
Gavin Beatty <gavinbeatty@gmail.com>
2012-07-03 20:44:42 +0200

    use MacVim's binaries by default on darwin

--- configs/common/bashrc.darwin.sh
+++ configs/common/bashrc.darwin.sh
@@ -10,6 +10,14 @@ fi
 if (test -d "/opt/local/sbin") && (! echo "${PATH:-}" | grep -Fq "/opt/local/sbin") ; then
     PATH="${PATH:-}${PATH:+:}/opt/local/sbin}" ; export PATH
 fi
+v_="${PATH:-}"
+n_="/Applications/MacVim.app/Contents/MacOS"
+if test -d "$n_" && ! echo "$v_" | grep -Fq "$n_" ; then
+    PATH="$n_${v_:+:}$v_" ; export PATH
+fi
+if type Vim >/dev/null 2>&1 ; then alias vim=Vim ; fi
+if type mvim >/dev/null 2>&1 ; then alias gvim=mvim ; fi
+
 # don't set MANPATH as it needs to contain all default manpage paths
 # as well as MacPort's
 v_="${SGML_CATALOG_FILES:-}"
