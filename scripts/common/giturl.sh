6f8c48701921b03c1977ea4f7cfe80b7ed062ee6
Gavin Beatty <gavinbeatty@gmail.com>
2012-07-17 17:06:36 +0200

    fix using git inside a repo acting on another via arg

--- scripts/common/giturl.sh
+++ scripts/common/giturl.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 # vi: set ft=sh expandtab tabstop=4 shiftwidth=4:
-main() {
-    test $# -ne 0 || set -- .
-    if test $# -eq 2 ; then
-        if test "$1" = "--" ; then
-            shift
-        fi
-    fi
-    if test $# -gt 1 ; then
-        die "Must give zero or one <path> arguments."
-    fi
-    if isgitrepo.sh "$1" ; then
-        (cd -- "$1" && git config remote.origin.url)
-    else
-        "$1 is not a git repository."
-    fi
-}
+set -e
+set -u
 trap " echo Caught SIGINT >&2 ; exit 1 ; " INT
 trap " echo Caught SIGTERM >&2 ; exit 1 ; " TERM
-main "$@"
+test $# -ne 0 || set -- .
+if test $# -eq 2 ; then
+    if test "$1" = "--" ; then
+        shift
+    fi
+fi
+test $# -ne 0 || set -- .
+if test $# -gt 1 ; then
+    die "Must give zero or one <path> arguments."
+fi
+(cd -- "$1" && if git log -1 -- >/dev/null 2>&1 ; then git config remote.origin.url ; else echo "error: $1 is not a git repository." >&2 ; exit 1 ; fi)
--- scripts/common/isgitrepo.sh
+++ scripts/common/isgitrepo.sh
@@ -4,15 +4,12 @@ set -e
 set -u
 trap " echo Caught SIGINT >&2 ; exit 1 ; " INT
 trap " echo Caught SIGTERM >&2 ; exit 1 ; " TERM
-if test $# -eq 2 ; then
-    if test x"$1" != x"--" ; then path="$1"
-    elif test x"$2" != x"--" ; then path="$2"
-    else path="$2" ; fi
-elif test $# -eq 0 ; then path="."
-elif test $# -eq 1 ; then path="$1"
-elif test $# -gt 2 ; then
-    echo "error: Expected zero or one <path> argument" >&2
-    echo "usage: $(basename -- "$0") [--] [<path>]" >&2
+if test $# -ne 0 ; then
+    if test "$1" = "--" ; then shift ; fi
+fi
+if test $# -eq 0 ; then set -- .
+elif test $# -gt 1 ; then
+    echo "error: Must give one or zero path arguments." >&2
     exit 1
 fi
-git log -1 -- "$path" >/dev/null 2>&1
+(cd -- "$1" && git log -1  >/dev/null 2>&1)
