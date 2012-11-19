c92937e4fe461e4217937ec0a85ae7b7a4a9cd44
Gavin Beatty <gavinbeatty@gmail.com>
2012-11-19 11:12:20 +0100

    dashinstall can rename, only handles dirs ending in /, used in Makefile

--- scripts/common/dashinstall.sh
+++ scripts/common/dashinstall.sh
@@ -6,9 +6,14 @@ trap ' echo Caught SIGTERM >&2 ; exit 1 ; ' TERM
 trap ' echo Unexpected exit >&2 ; exit 1 ; ' 0
 prog="$(basename -- "$0")"
 pax() { trap '' 0 ; exit 0 ; }
-usage() { echo "usage: $prog <absdash> <shellscript>... <destination>" ; }
-die() { trap '' 0 ; echo "error: $@" >&2 ; exit 1 ; }
-udie() { trap '' 0 ; usage >&2 ; echo "error: $@" >&2 ; exit 1 ; }
+usage() { echo "usage: $prog [-m <mod>] <absdash> <shellscript>... <destination>" ; }
+die() { echo "error: $@" >&2 ; pax 1 ; }
+udie() { usage >&2 ; die "$@" ; }
+if test $# -gt 0 && test "$1" = "-m" ; then
+    if test $# -eq 1 ; then udie "Option -m must have a <mod>." ; fi
+    mod="$2"
+    shift 2
+fi
 test $# -gt 2 || udie "Not enough arguments."
 absdash="$1"
 shift
@@ -19,17 +24,19 @@ case "$absdash" in
 esac
 last() { local i= ; local f= ; for i in "$@" ; do f="$i" ; done ; echo "$f" ; }
 dest="$(last "$@")"
-if test "$dest" != "/" && test "$dest" != "//" ; then dest="${dest%%/}" ; fi
 prevset=
 for i in "$@" ; do
     # operate on prev so last is skipped
     if test -n "$prevset" ; then
         base="$(basename -- "$prev")"
+        out="$dest"
+        case "$dest" in */) out="${dest}$base" ;; esac
         if test "$(head -n 1 -- "$prev")" = "#!/bin/sh" ; then
-            sed -e "1s!^#\!/bin/sh\$!#\!${absdash}!" "$prev" | tee -- "${dest}/$base" >/dev/null
+            sed -e "1s!^#\!/bin/sh\$!#\!${absdash}!" "$prev" | tee -- "$out" >/dev/null
         else
-            cat -- "$prev" | tee -- "${dest}/$base" >/dev/null
+            cat -- "$prev" | tee -- "$out" >/dev/null
         fi
+        if test -n "$mod" ; then chmod "$mod" -- "$out" ; fi
     fi
     prev="$i"
     prevset=1
