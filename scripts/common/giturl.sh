14c8ec5a99d00bfa55193626ad244a8977521990
Gavin Beatty <gavinbeatty@gmail.com>
2012-12-03 14:13:37 +0100

    fix giturl and isgitrepo

--- scripts/common/giturl.sh
+++ scripts/common/giturl.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 # vi: set ft=sh expandtab tabstop=4 shiftwidth=4:
-main() {
-    test $# -ne 0 || set -- .
-    if test $# -eq 2 ; then
-        if test "$1" = "--" ; then
-            shift
-        fi
-    fi
-    if test $# -gt 1 ; then
-        die "Must give zero or one <path> arguments."
-    fi
-    if isgitrepo.sh "$1" ; then
-        (cd -- "$1" && git config remote.origin.url)
-    else
-        "$1 is not a git repository."
-    fi
-}
+set -e
+set -u
 trap " echo Caught SIGINT >&2 ; exit 1 ; " INT
 trap " echo Caught SIGTERM >&2 ; exit 1 ; " TERM
-main "$@"
+git="${GIT:-git}"
+test $# -ne 0 || set -- .
+if test $# -eq 2 ; then
+    if test "$1" = "--" ; then
+        shift
+    fi
+fi
+if test $# -gt 1 ; then
+    echo "Must give zero or one <path> arguments." >&2 ; exit 1
+fi
+if $git log -1 -- "$1" >/dev/null 2>&1 "$1" ; then
+    (cd -- "$1" && $git config remote.origin.url)
+else
+    echo "$1 is not a git repository." >&2 ; exit 1
+fi
