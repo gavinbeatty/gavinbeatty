7c6040d04f52c00f9af025054ae9b624bd37069c
Gavin Beatty <gavinbeatty@gmail.com>
2012-12-17 15:57:33 +0100

    get rid of broken keychain kill in bash_logout

--- configs/common/bash_logout.sh
+++ configs/common/bash_logout.sh
@@ -3,11 +3,13 @@ if test -n "${bash_logout_guard-}" ; then return 0 ; fi
 bash_logout_guard=1
 #iecho ".bash_logout"
 
-if (type -- keychain >/dev/null 2>&1) \
-&& (type -- who >/dev/null 2>&1) && (type -- wc >/dev/null 2>&1) \
-&& (test "$(who | grep -F -- "$USER" | wc -l)" -eq 1) ; then
-    keychain --quiet -k all
-fi
+# XXX this does not work when using tmux etc. -- you'll only have one login shell
+# XXX tmux list-sessions hangs for some reason when run from ~/.bash_logout
+#if (type -- keychain >/dev/null 2>&1) \
+#&& (type -- who >/dev/null 2>&1) && (type -- wc >/dev/null 2>&1) \
+#&& (test "$(who | grep -F -- "$USER" | wc -l)" -eq 1) ; then
+#    keychain --quiet -k all
+#fi
 
 for bash_logout_i in $HOSTS ; do
     if test -r "${HOME}/.bash_logout.${bash_logout_i}" ; then
