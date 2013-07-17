729168ba945fc231dc0d60f8646a31751a48ef37
Gavin Beatty <gavinbeatty@gmail.com>
2013-07-17 09:52:18 -0500

    use proper sh default

--- scripts/common/svndiffadaptor.sh
+++ scripts/common/svndiffadaptor.sh
@@ -2,7 +2,7 @@
 # vi: set ft=sh expandtab shiftwidth=4 tabstop=4:
 set -e
 set -u
-DIFF="${DIFF-colordiff}"
+DIFF="${DIFF:-colordiff}"
 DIFFCTX="${DIFFCTX:-10}"
 DIFFOPTS="${DIFFOPTS:--U$DIFFCTX -p}"
 DIFFEXTRA="${DIFFEXTRA:-}"
