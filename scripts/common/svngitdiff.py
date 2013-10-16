c575416bd5209adc54fd2df130624c43264c1cb0
Gavin Beatty <gavinbeatty@gmail.com>
2013-10-16 11:33:18 -0500

    support stdin, no-ext-diff, docs, lenient on stat

--- scripts/common/svngitdiff.py
+++ scripts/common/svngitdiff.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # vi: set ft=python et sw=2 ts=2:
-from os import environ
-import sys
+from os import environ, fstat
+from stat import S_ISFIFO
 import subprocess as sp
+import sys
+
+"""Allows use of `git diff --no-index` with svn.
+
+There are many motivating `git diff` features to warrant this:
+
+  - easy colorize diff (`--color=always, `--color=auto`)
+  - automatic color-warning for trailing whitespace
+  - patience diff (`--patience`)
+  - color word diff (`--color-words`, `--word-diff=color`)
+  - full function context (`--function-context`, `-W`)
+
+To use with svn:
+
+  - add svngitdiff.py to your PATH
+  - configure `~/.subversion/config` (on Unix-alikes) like so:
+
+      [helpers]
+      diff-cmd = svngitdiff.py
+"""
 
 def ExtractL(args):
   """A simple attempt to extract -L "label" and -L"label".
@@ -36,31 +56,53 @@ def ExtractL(args):
         new_args.append(arg)
   return (new_args, ells)
 
+def DiffIndex(lines):
+  """Returns the index of the first line after the '---' and '+++' lines.
+
+  There may be no such lines because:
+  - the patterns were not found within a few lines
+  - `len(lines)` is too small
+
+  In which case, -1 (negative one) is returned.
+  """
+  size = len(lines)
+  # Inspect a decent number of prefix lines, so that we can skip over any `--stat` lines.
+  for i, line in enumerate(lines[:50]):
+    if i + 2 < size and line.find('---') >= 0 and lines[i + 1].find('+++') >= 0:
+      return i + 2
+  return -1
+
+def FDIsPipe(fd):
+  return S_ISFIFO(fstat(fd).st_mode)
+
 try:
   args, ells = ExtractL(sys.argv[1:])
   git = environ.get('GIT_EXE', 'git')
-  cmd = [git, 'diff', '--no-index'] + args
+  # Use --no-ext-diff: if you want to use a different diff tool,
+  # then configure it with svn yourself.
+  # No sense in "proxying" many times to the underlying diff tool.
+  cmd = [git, 'diff', '--no-index', '--no-ext-diff'] + args
   p = sp.Popen(cmd, stdout=sp.PIPE, stderr=sp.PIPE)
-  stdout, stderr = p.communicate()
+  stdin = None
+  if FDIsPipe(sys.stdin.fileno()):
+    stdin = sys.stdin.read()
+  stdout, stderr = p.communicate(input=stdin)
 except OSError, e:
   sys.exit(str(e))
 # Print all of stdout, then all of stderr.
-if len(ells) < 2:
-  sys.stdout.write(stdout)
+# Lines 0 and 1 are diff and sha1 noise.
+# Lines 2 and 3 are where the labels go.
+# This way takes care of --color=always.
+# Don't try to colorize new '---' lines.
+lines = stdout.splitlines()
+idx = DiffIndex(lines)
+if idx >= 0 and len(ells) >= 2:
+  print('--- ' + ells[0])
+  print('+++ ' + ells[1])
+  for line in lines[idx:]:
+    print(line)
 else:
-  for i, line in enumerate(stdout.splitlines()):
-    # Lines 0 and 1 are diff and sha1 noise.
-    # Lines 2 and 3 are where the labels go.
-    # This way takes care of --color=always.
-    # Don't try to colorize the '---' lines.
-    if i == 2:
-      assert line.find('---') >= 0, 'Line: ' + line
-      print('--- ' + ells[0])
-    elif i == 3:
-      assert line.find('+++') >= 0, 'Line: ' + line
-      print('+++ ' + ells[1])
-    elif i >= 4:
-      print line
-if stderr.rstrip('\n'):
+  sys.stdout.write(stdout)
+if stderr.rstrip('\r\n'):
   sys.stderr.write(stderr)
 sys.exit(p.returncode)
