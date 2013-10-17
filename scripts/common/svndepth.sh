0fbde9aaeb9aab89603c51296c954da22f4277e5
Gavin Beatty <gavinbeatty@gmail.com>
2013-10-17 10:05:53 -0500

    `svndepth.sh d` := `svn up --set-depth depth`

---
+++ scripts/common/svndepth.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# vi: set ft=sh et sw=2 ts=2:
+set -e
+set -u
+die() { printf "%s\n" "$*" >&2 ; exit 1 ; }
+usage() { printf "usage: %s [-0] <depth> [<directory>...]\n" "$(basename "$0")" ; }
+SVN_EXE="${SVN_EXE:-svn}"
+depth="$1" ; shift
+case "$depth" in
+  a|all|full|inf|infinite|infinity) depth=infinity ;;
+  e|empty|none) depth=empty ;;
+  f|files) depth=files ;;
+  im|imm|immediate|immediates) depth=immediates ;;
+  *) die "Invalid <depth>, $depth" ;;
+esac
+${SVN_EXE} update --set-depth "$depth" "$@"
