e72ead1101c0a69b12541cb059b19eddbad0b7b0
Gavin Beatty <gavinbeatty@gmail.com>
2015-04-30 15:52:03 -0500

    rejig so there can be labels etc...

--- scripts/common/kde-plot.py
+++ scripts/common/kde-plot.py
@@ -3,12 +3,38 @@ import sys
 import numpy as np
 from scipy.stats import kde
 import matplotlib.pyplot as plt
-if not sys.argv[1:]:
-    sys.exit("usage: kde-plot.py <data-file> <bins>")
-try:
-    bins = int(sys.argv[2])
-except IndexError:
-    bins = 100
+if len(sys.argv) < 2:
+    sys.exit("usage: kde-plot.py <data-file> [<xlabel>] [<ylabel>] [<title>] [<bins>] [<normed>]")
+def to_bool(x):
+    x = x.lower()
+    if x in ('0', 'false', 'no'):
+        return False
+    if x in ('1', 'true', 'yes'):
+        return True
+    raise Exception('Unrecognized value for <normed>: ' + x)
+xlabel = None
+ylabel = None
+title = None
+histkws = {'normed': True, 'bins': 100}
+plotkws = {'antialiased': True}
+path = sys.argv[1]
+if path != '-':
+    title = path
+if len(sys.argv) > 2 and sys.argv[2]:
+    xlabel = sys.argv[2]
+if len(sys.argv) > 3 and sys.argv[3]:
+    ylabel = sys.argv[3]
+if len(sys.argv) > 4 and sys.argv[4]:
+    title = sys.argv[4]
+if len(sys.argv) > 5:
+    histkws['bins'] = int(sys.argv[5])
+if len(sys.argv) > 6:
+    histkws['normed'] = to_bool(sys.argv[6])
+if not ylabel:
+    if histkws['normed']:
+        ylabel = 'normalized samples'
+    else:
+        ylabel = 'samples'
 def cli_open(path, *args, **kwargs):
     if path == '-':
         return sys.stdin
@@ -23,10 +49,13 @@ def cli_open(path, *args, **kwargs):
         return lzma.open(path, *args, **kwargs)
     else:
         return open(path, *args, **kwargs)
-with cli_open(sys.argv[1], 'r') as f:
+with cli_open(path, 'r') as f:
     x = np.asarray([float(line) for line in f])
 density = kde.gaussian_kde(x)
 xgrid = np.linspace(x.min(), x.max(), 100)
-plt.hist(x, bins=bins, normed=True)
-plt.plot(xgrid, density(xgrid), 'r-')
+plt.hist(x, **histkws)
+if xlabel: plt.xlabel(xlabel)
+if ylabel: plt.ylabel(ylabel)
+if title: plt.title(title)
+plt.plot(xgrid, density(xgrid), 'r-', **plotkws)
 plt.show()
