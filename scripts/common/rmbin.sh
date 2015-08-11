84a6432f65f71da22e8da8423ff35cf245fad3a7
Gavin Beatty <gavinbeatty@gmail.com>
2015-08-11 13:32:39 -0500

    Handle VS C# projects and allow dry run

--- scripts/common/rmbin.sh
+++ scripts/common/rmbin.sh
@@ -9,6 +9,7 @@ getopt=${GETOPT-getopt}
 xargs="${XARGS:-}"
 help=${RMBIN_HELP:-}
 quiet=${RMBIN_QUIET:-}
+dryrun=${RMBIN_DRY_RUN:-}
 variant=${RMBIN_VARIANT:-}
 symlinks="${RMBIN_SYMLINKS:--L}"
 have() { type "$@" >/dev/null 2>&1 ; }
@@ -16,7 +17,7 @@ die() { echo "error: $@" ; exit 1 ; }
 usage() {
     cat <<EOF
 usage: $prog -h
-   or: $prog [-b <variant>] [-q] [-L|-P|-H|-D] [<directory>]
+   or: $prog [-b <variant>] [-q] [-n] [-L|-P|-H|-D] [<directory>]
 EOF
 }
 getopt_works() {
@@ -24,13 +25,14 @@ getopt_works() {
 }
 main() {
     if getopt_works ; then
-        opts="$("$getopt" -n "$prog" -o "hqb:LPHD" -- "$@")"
+        opts="$("$getopt" -n "$prog" -o "hqnb:LPHD" -- "$@")"
         eval set -- "$opts"
 
         while test $# -gt 0 ; do
 			case "$1" in
             -h) help=1 ;;
             -q) quiet=1 ;;
+            -n) dryrun=1 ;;
             -b) variant=$2 ; shift ;;
             -L) symlinks="-L" ;;
             -P) symlinks="-P" ;;
@@ -58,12 +60,13 @@ main() {
     if test $# -eq 0 ; then
         set -- .
     fi
-    sh="just() { \"\$@\" ; } ; echodo() { printf \"%s\\n\" \"\$*\" ; \"\$@\" ; } ; for i in \"\$@\" ; do if ! printf \"%s\\n\" \"\$i\" | grep -q \"/x_\\(papi\\|ums\\|boost\\|xerces\\|google\\|gmock\\|gtest\\|protobuf\\)\" ; then \"\${0:-just}\" rm -r -- \"\$i\" ; fi ; done"
+    sh="just() { \"\$@\" ; } ; quote() { local l= ; local i= ; for i in \"\$@\"; do printf '%s%q' \"\$l\" \"\$i\" ; l=' ' ; done ; echo ; } ; echodo() { quote \"\$@\" ; \"\$@\" ; } ; for i in \"\$@\" ; do if ! printf %s\\\\n \"\$i\" | grep -q \"/x_\\(papi\\|ums\\|boost\\|xerces\\|google\\|gmock\\|gtest\\|protobuf\\)\" ; then \"\${0:-just}\" rm -r -- \"\$i\" ; fi ; done"
     do=echodo
     test -z "$quiet" || do=just
+    test -z "$dryrun" || do=quote
     if test -z "$variant" ; then
         test -n "$quiet" || set -x
-        find $symlinks "$@" -type d \( -name 'bin' -o -name 'bin.v2' -o -iname 'debug' -o -iname 'release' \) -prune -print0 | $xargs -r0 sh -c "$sh" "$do"
+        find $symlinks "$@" -type d \( -name 'bin' -o -name 'bin.v2' -o -name 'obj' -o -iname 'debug' -o -iname 'release' \) -prune -print0 | $xargs -r0 sh -c "$sh" "$do"
     else
         test -n "$quiet" || set -x
         find $symlinks "$@" -type d -name "$variant" -prune -print0 | $xargs -r0 sh -c "$sh" "$do"
